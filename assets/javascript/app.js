7389937956a0541bb71fc49e4815916db24dad7e
Matt Colson <mc180g@gmail.com>
2019-11-05 19:06:00 -0500

    damn

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,81 @@
-$(document).ready(function () {
+var breweries;
+var lat;
+var long;
 
-    $("#map").hide();
+$(document).ready(function() {
+    $(".row").hide();
+    $("#add-brew").on("click", function(event) {
 
-    $("#add-brew").on("click", function (event) {
         event.preventDefault();
-        let userName = "by_name=" + $("#brewery-name").val().trim();
-        let userCity = "by_city=" + $("#city").val().trim();
-        let userState = "by_state=" + $("#state").val().trim();
-
-
-        if(userName == "" && userCity == "" && userState == ""){
-            prompt("Must fill out at least 1 parameter");
-        } else
+        $("#brewery").empty();
+        $(".row").show();
+        
+        let userName = "&by_name=" + $("#brewery-name").val().trim();
+        let userState = "&by_state=" + $("#state").val().trim();
+        let userCity = "&by_city=" + $("#city").val().trim();
+        $("#brewery-name").val("");
+        $("#state").val("");
+        $("#city").val("");
+        let queryURL = "https://api.openbrewerydb.org/breweries?per_page=20";
+        if (userName === "&by_name=") {
+            userName = "";
+        }
+        if (userCity === "&by_city=") {
+            userCity = ""
+        }
+        if (userState === "&by_state=") {
+            userState = ""
+        }
+        if (userCity === "&by_city=" && userName === "&by_state=") {
+            userCity = ""
+            userState = ""
+        }
+        if (userState === "&by_state=" && userName === "&by_name=") {
+            userName = "";
+            userState = "";
+        }
+        if (userCity === "&by_city=" && userState === "&by_state=") {
+            userCity = "";
+            userState = "";
+        }
+        if (userName === "" && userCity === "" && userState === "") {
+            alert("Please fill out at least 1 parameter");
+            return false;
+        }
 
         $.ajax({
-            url: "https://api.openbrewerydb.org/breweries?" + userCity,
+            url: queryURL + userName + userState + userCity,
             method: "GET"
-        }).then(function (response) {
-            console.log(response);
-
-        // //     $.ajax({
-        // //         url: "https://api.openbrewerydb.org/breweries?by_state=" + userState,
-        // //         method: "GET"
-        // //     }).then(function (response) {
-        // //         console.log(response);
-
-        //         $.ajax({
-        //             url: "https://api.openbrewerydb.org/breweries?" + userName || userCity || userState,
-        //             method: "GET"
-        //         }).then(function (response) {
-        //             console.log(response);
+        }).then(function(response) {
 
 
+            if (Array.isArray(response) && !response.length) {
+                alert("Search term not found");
+                return false;
+            }
 
+            breweries = response;
+            console.log(breweries);
             for (let i = 0; i < response.length; i++) {
-
-
-
+                let brewRow = $("<div class='row'>");
                 let brewDiv = $("<div>");
+                let mapDiv = $("<div id='map" + i + "'>")
+                mapDiv.addClass("map");
+                let name = $("<h3>").text(response[i].name);
+                name.attr("data-lat", response[i].latitude);
+                name.attr("data-long", response[i].longitude);
+              
 
-                let name = $("<button>").text(response[i].name);
                 let brewCity = $("<p>").text(response[i].city);
                 let brewState = $("<p>").text(response[i].state);
                 let address = $("<p>").text(response[i].street);
                 let zip = $("<p>").text(response[i].postal_code);
-                let website = $("<p>").text(response[i].website_url);
+                let website = $("<a href=''>").text(response[i].website_url);
                 let phone = $("<p>").text(response[i].phone);
+                let lat = response[i].latitude;
+                let long = response[i].longitude;
+                console.log(lat);
+                console.log(long);
                 brewDiv.addClass("brewery-div");
                 name.addClass("brw-btn");
                 brewDiv.append(name);
@@ -55,17 +85,47 @@ $(document).ready(function () {
                 brewDiv.append(zip);
                 brewDiv.append(website);
                 brewDiv.append(phone);
+                brewDiv.append(lat);
+                brewDiv.append(long);
 
-                $("#breweries-go-here").append(brewDiv);
+                $(brewRow).append(brewDiv);
+                $(brewRow).append(mapDiv);
+                $("#brewery").append(brewRow);
+
+                let platform = new H.service.Platform({
+                    "app_id": "dyibLlBU2QNaCv7xikm2",
+                    "apikey": "5nf_6CsIndRsth2qYd4s86AwOQs8XMDgUf7vOLU09Ls"
+                });
+                let maptypes = platform.createDefaultLayers();
+
+                let map = new H.Map(
+                    document.getElementById("map" + i),
+                    maptypes.vector.normal.map, {
+                        zoom: 10,
+                        center: { lat: lat, lng: long },
+                    }
+                );
+                
+                const mapEvents = new H.mapevents.MapEvents(map);
+                const behavior = new H.mapevents.Behavior(mapEvents);
+                let svgMarkup = '<svg width="24" height="24" ' +
+                    'xmlns="http://www.w3.org/2000/svg">' +
+                    '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+                    'height="22" /><text x="12" y="18" font-size="12pt" ' +
+                    'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+                    'fill="white">H</text></svg>';
+                let icon = new H.map.Icon(svgMarkup),
+                    coords = { lat: lat, lng: long },
+                    marker = new H.map.Marker(coords, { icon: icon });
+                map.addObject(marker);
+                map.setCenter(coords);
+
             };
-
         });
-
     });
-    //     });
-    // });
 
 
+<<<<<<< HEAD
     $("#breweries-go-here").on("click",".brw-btn", function () {
         console.log("is this working")
         $("#breweries-go-here").hide();
@@ -104,3 +164,8 @@ $(document).ready(function () {
 
 })
 
+=======
+});
+
+
+>>>>>>> 488cbe3c67c4b524f8a4df35cd38aecffc29e009
